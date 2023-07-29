bbe87e163ab324aec59fbd04c57faa8e03541491
Arman-Kabir <armankabir8@gmail.com>
2023-07-30 01:13:20 +0600

    Programmatically navigating

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,11 @@
+import { useRouter } from "next/router";
 
 const ErrorPage = () => {
+    const router = useRouter();
+    setTimeout(()=>{
+        router.push("/");
+    },5000);
+    
   return (
     <div>
         <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg" alt="" width='100%' />

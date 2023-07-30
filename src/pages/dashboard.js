3d130a05f02422233630947bb390ef777ec845d5
Arman-Kabir <armankabir8@gmail.com>
2023-07-30 11:23:11 +0600

    LAyout explored

---
+++ src/pages/dashboard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>Dashboard Homepage</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
+
+Dashboard.getLayout = function getLayout(page){
+    return (
+        <DashboardLayout>
+            {page}
+        </DashboardLayout>
+    )
+}

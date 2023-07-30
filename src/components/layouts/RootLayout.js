d7a154a6c64602ba42e390b69dbf348ff8b49f97
Arman-Kabir <armankabir8@gmail.com>
2023-07-30 18:50:40 +0600

    menu half done

--- src/components/layouts/RootLayout.js
+++ src/components/layouts/RootLayout.js
@@ -1,6 +1,44 @@
 
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Dropdown, Layout, Menu, Space, theme } from 'antd';
 const { Header, Content, Footer } = Layout;
+import { AppstoreOutlined, MailOutlined, SettingOutlined, SmileOutlined } from '@ant-design/icons';
+
+const items = [
+    {
+        key: '1',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                1st menu item
+            </a>
+        ),
+    },
+    {
+        key: '2',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+                2nd menu item (disabled)
+            </a>
+        ),
+        icon: <SmileOutlined />,
+        disabled: true,
+    },
+    {
+        key: '3',
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+                3rd menu item (disabled)
+            </a>
+        ),
+        disabled: true,
+    },
+    {
+        key: '4',
+        danger: true,
+        label: 'a danger item',
+    },
+];
+
+
 
 const RootLayout = ({ children }) => {
     const {
@@ -13,41 +51,36 @@ const RootLayout = ({ children }) => {
                     style={{
                         display: 'flex',
                         alignItems: 'center',
+                        justifyContent: 'space-between'
                     }}
                 >
-                    <div className="demo-logo" />
+                    <div className="demo-logo" style={{ color: "white", fontWeight: "bold", fontSize: "30px" }}>
+                        PC-Maker
+                    </div>
                     <Menu
                         theme="dark"
                         mode="horizontal"
                         defaultSelectedKeys={['2']}
-                        items={new Array(15).fill(null).map((_, index) => {
-                            const key = index + 1;
-                            return {
-                                key,
-                                label: `nav ${key}`,
-                            };
-                        })}
-                    />
+
+                    >
+                        Categories
+                    </Menu>
+
+                    <Dropdown menu={{items}}>
+                        <Space>Hello</Space>
+                    </Dropdown>
                 </Header>
                 <Content
                     style={{
                         padding: '0 50px',
                     }}
                 >
-                    <Breadcrumb
-                        style={{
-                            margin: '16px 0',
-                        }}
-                    >
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
+
                     <div
                         className="site-layout-content"
                         style={{
                             background: colorBgContainer,
-                            height:"100vh"
+                            height: "100vh"
                         }}
                     >
                         {children}

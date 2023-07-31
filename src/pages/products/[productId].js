443b0480a49d81209c57f4038ce925e275e77ca3
Arman-Kabir <armankabir8@gmail.com>
2023-07-31 11:47:26 +0600

    get static paths half done

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -1,10 +1,45 @@
+import RootLayout from "@/components/layouts/RootLayout";
 import { useRouter } from "next/router";
 
-const ProductDetail = () => {
+const ProductDetail = ({ product }) => {
     const router = useRouter();
-  return (
-    <div>ProductDetail: {router.query.productId}</div>
-  )
+    return (
+        <div>
+            <div>ProductDetail: {router.query.productId}</div>
+            <div>Name {product?.name}</div>
+        </div>
+    )
 }
 
 export default ProductDetail;
+
+ProductDetail.getLayout = function getLayout(page) {
+    return (
+        <RootLayout>
+            {page}
+        </RootLayout>
+    )
+};
+
+export const getStaticPaths = async()=>{
+    const res = await fetch("http://localhost:5000");
+    const products = await res.json();
+
+    const paths = products.map((product)=>({
+        params:{id:product.id}
+    }))
+
+    return {paths,fallback:false}
+}
+
+export const getStaticProps = async () => {
+
+    const res = await fetch("http://localhost:5000/products/1");
+    const data = await res.json();
+
+    return {
+        props: {
+            product: data,
+        },
+    }
+}

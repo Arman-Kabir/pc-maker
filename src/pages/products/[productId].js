371a225f376bd9b7ad2663207168851b12290911
Arman-Kabir <armankabir8@gmail.com>
2023-07-31 15:40:10 +0600

    server side props implemented

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -21,22 +21,30 @@ ProductDetail.getLayout = function getLayout(page) {
     )
 };
 
-export const getStaticPaths = async()=>{
-    const res = await fetch("http://localhost:5000/products");
-    const products = await res.json();
+// export const getStaticPaths = async()=>{
+//     const res = await fetch("http://localhost:5000/products");
+//     const products = await res.json();
+//     const paths = products.map((product)=>({
+//         params:{productId:product.id}
+//     }))
+//     return {paths,fallback:false}
+// }
 
-    const paths = products.map((product)=>({
-        params:{productId:product.id}
-    }))
+// export const getStaticProps = async (context) => {
+//     const {params} = context;
+//     const res = await fetch(`http://localhost:5000/products/${params.productId}`);
+//     const data = await res.json();
+//     return {
+//         props: {
+//             product: data,
+//         },
+//     }
+// }
 
-    return {paths,fallback:false}
-}
-
-export const getStaticProps = async (context) => {
+export const getServerSideProps = async (context) => {
     const {params} = context;
     const res = await fetch(`http://localhost:5000/products/${params.productId}`);
     const data = await res.json();
-
     return {
         props: {
             product: data,

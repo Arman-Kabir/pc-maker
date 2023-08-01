fd5740bd6f3a39c674e82060a74a86c25e01a0c4
Arman-Kabir <armankabir8@gmail.com>
2023-08-01 21:42:35 +0600

    pc-builder page converted using ant grid system

--- src/pages/pc-builder/index.js
+++ src/pages/pc-builder/index.js
@@ -9,40 +9,43 @@ const PcBuilder = () => {
     const components = useSelector((state) => state.components.components);
     // console.log(components);
     return (
-        <div style={{ display: 'grid',gridAutoColumns:'1fr 1fr' }}>
+        <div>
+            <Row gutter={[16, 16]}>
+                <Col span={12}>
 
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', margin: '10px 0', width: '500px' }}>
-
-                <Link href='/pc-builder/processor'><Card hoverable style={{ borderColor: 'crimson', width: '500px' }}>Processor <Button type='primary'>Choose</Button> </Card></Link>
-
-
-                <Link href='/pc-builder/motherboard'><Card hoverable style={{ borderColor: 'crimson' }}>Motherboard <Button type='primary'>Choose</Button></Card></Link>
-
-                <Link href='/pc-builder/ram'><Card hoverable style={{ borderColor: 'crimson' }}>Ram  <Button type='primary'>Choose</Button></Card></Link>
-
-                <Link href='/pc-builder/power-supply-unit'><Card hoverable style={{ borderColor: 'crimson' }}>Power Supply Unit  <Button type='primary'>Choose</Button></Card></Link>
-
-                <Link href='/pc-builder/storage-device'><Card hoverable style={{ borderColor: 'crimson' }}>Storage Device  <Button type='primary'>Choose</Button></Card></Link>
-
-                <Link href='/pc-builder/monitor'><Card hoverable style={{ borderColor: 'crimson' }}>Monitor  <Button type='primary'>Choose</Button></Card></Link>
-
-                <Link href='/category/others'><Card hoverable style={{ borderColor: 'crimson' }}>Others  <Button type='primary'>Choose</Button></Card></Link>
-
-            </div>
+                    <Link href='/pc-builder/processor'><Card hoverable style={{ borderColor: 'crimson', }}>Processor <Button type='primary'>Choose</Button> </Card></Link>
 
 
+                    <Link href='/pc-builder/motherboard'><Card hoverable style={{ borderColor: 'crimson' }}>Motherboard <Button type='primary'>Choose</Button></Card></Link>
 
-            <div>
-                {
-                    components.map((component) => (
-                        <PcBuilderComponent key={component.id} component={component}>Hello</PcBuilderComponent>
-                       
-                    ))
-                }
-            </div>
+                    <Link href='/pc-builder/ram'><Card hoverable style={{ borderColor: 'crimson' }}>Ram  <Button type='primary'>Choose</Button></Card></Link>
+
+                    <Link href='/pc-builder/power-supply-unit'><Card hoverable style={{ borderColor: 'crimson' }}>Power Supply Unit  <Button type='primary'>Choose</Button></Card></Link>
+
+                    <Link href='/pc-builder/storage-device'><Card hoverable style={{ borderColor: 'crimson' }}>Storage Device  <Button type='primary'>Choose</Button></Card></Link>
+
+                    <Link href='/pc-builder/monitor'><Card hoverable style={{ borderColor: 'crimson' }}>Monitor  <Button type='primary'>Choose</Button></Card></Link>
+
+                    <Link href='/category/others'><Card hoverable style={{ borderColor: 'crimson' }}>Others  <Button type='primary'>Choose</Button></Card></Link>
+
+                </Col>
+
+                <Col span={12}>
+                    {
+                        components.map((component) => (
+                            // <PcBuilderComponent key={component.id} component={component}>Hello</PcBuilderComponent>
+                            <Card key={component.id}>
+                                <p>{component.name}</p>
+                            </Card>
+
+                        ))
+                    }
+                </Col>
+            </Row>
 
 
-
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', margin: '10px 0', width: '500px' }}></div>
+            <div></div>
         </div>
     )
 }

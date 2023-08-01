b704b7834403fd98ad63fd0ab567659afd0fe701
Arman-Kabir <armankabir8@gmail.com>
2023-08-01 22:41:15 +0600

    select components styled

--- src/pages/pc-builder/index.js
+++ src/pages/pc-builder/index.js
@@ -1,51 +1,72 @@
 import PcBuilderComponent from '@/components/UI/PcBuilderComponent'
 import RootLayout from '@/components/layouts/RootLayout'
 import { Button, Card, Col, Row, Space } from 'antd'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+
+const categoryCardStyle = {
+    height: '50px',
+    margin: '5px 0px',
+    // backgroundColor:'black',
+    // color:'white',
+    // borderColor: 'crimson',
+    display: 'flex',
+    alignItems: 'center',
+}
+const selectedCardStyle = {
+    height: '100px',
+    margin: '5px 0px',
+    // backgroundColor:'black',
+    // color:'white',
+    // borderColor: 'crimson',
+    display: 'flex',
+    alignItems: 'center',
+}
 
 const PcBuilder = () => {
     const components = useSelector((state) => state.components.components);
+    const count = useSelector((state) => state.components.count);
+
+    const categories = ['processor', 'motherboard', 'ram', 'power-supply-unit', 'storage-device', 'monitor', 'others'];
     // console.log(components);
     return (
         <div>
+            <h1 style={{textAlign:'center',margin:'20px'}}>PC BUILDER Page</h1>
             <Row gutter={[16, 16]}>
                 <Col span={12}>
+                    <h2 style={{textAlign:'center',margin:'20px'}}>Select Components</h2>
+                    {
+                        categories.map((category) => (
+                            // console.log(category)
+                            <Row gutter={[16, 16]} key={category}>
+                                <Col span={12}>
+                                    <Card hoverable style={categoryCardStyle}>
+                                        <span style={{ fontWeight: 'bold', paddingRight: '25px',textTransform:'uppercase' }}>{category}</span>
 
-                    <Link href='/pc-builder/processor'><Card hoverable style={{ borderColor: 'crimson', }}>Processor <Button type='primary'>Choose</Button> </Card></Link>
-
-
-                    <Link href='/pc-builder/motherboard'><Card hoverable style={{ borderColor: 'crimson' }}>Motherboard <Button type='primary'>Choose</Button></Card></Link>
-
-                    <Link href='/pc-builder/ram'><Card hoverable style={{ borderColor: 'crimson' }}>Ram  <Button type='primary'>Choose</Button></Card></Link>
-
-                    <Link href='/pc-builder/power-supply-unit'><Card hoverable style={{ borderColor: 'crimson' }}>Power Supply Unit  <Button type='primary'>Choose</Button></Card></Link>
-
-                    <Link href='/pc-builder/storage-device'><Card hoverable style={{ borderColor: 'crimson' }}>Storage Device  <Button type='primary'>Choose</Button></Card></Link>
-
-                    <Link href='/pc-builder/monitor'><Card hoverable style={{ borderColor: 'crimson' }}>Monitor  <Button type='primary'>Choose</Button></Card></Link>
-
-                    <Link href='/category/others'><Card hoverable style={{ borderColor: 'crimson' }}>Others  <Button type='primary'>Choose</Button></Card></Link>
-
+                                        <Button type='primary'><Link href={`/pc-builder/${category}`}>Choose</Link></Button>
+                                    </Card>
+                                </Col>                              
+                            </Row>
+                        ))
+                    }
                 </Col>
 
                 <Col span={12}>
+                    <h2 style={{textAlign:'center',margin:'20px'}}>Selected Components: {count} selected</h2>
                     {
                         components.map((component) => (
                             // <PcBuilderComponent key={component.id} component={component}>Hello</PcBuilderComponent>
-                            <Card key={component.id}>
+
+                            <Card key={component.id} style={selectedCardStyle}>
+                                <Image src={component.image} width={'100'} height={'100'} alt='component-image'></Image>
                                 <p>{component.name}</p>
                             </Card>
-
                         ))
                     }
                 </Col>
             </Row>
-
-
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', margin: '10px 0', width: '500px' }}></div>
-            <div></div>
         </div>
     )
 }

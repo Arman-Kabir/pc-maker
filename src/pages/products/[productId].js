f45ae748476960ab62c6e2255bab4b7589c7d4d6
Arman-Kabir <armankabir8@gmail.com>
2023-08-02 15:50:09 +0600

    single product show getStaticPaths problems solved

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -21,16 +21,28 @@ ProductDetail.getLayout = function getLayout(page) {
     )
 };
 
-// export const getStaticPaths = async()=>{
-//     const res = await fetch("http://localhost:5000/products");
-//     const products = await res.json();
-//     const paths = products.map((product)=>({
-//         params:{productId:product.id}
-//     }))
-//     return {paths,fallback:false}
-// }
+export const getStaticPaths = async()=>{
+    const res = await fetch("http://localhost:3000/api/products");
+    const products = await res.json();
 
-// export const getStaticProps = async (context) => {
+    const paths = products.data.map((product)=>({
+        params:{productId:product.id}
+    }))
+    return {paths,fallback:false}
+}
+
+export const getStaticProps = async (context) => {
+    const {params} = context;
+    const res = await fetch(`http://localhost:3000/api/products/${params.productId}`);
+    const data = await res.json();
+    return {
+        props: {
+            product: data,
+        },
+    }
+}
+
+// export const getServerSideProps = async (context) => {
 //     const {params} = context;
 //     const res = await fetch(`http://localhost:5000/products/${params.productId}`);
 //     const data = await res.json();
@@ -40,14 +52,3 @@ ProductDetail.getLayout = function getLayout(page) {
 //         },
 //     }
 // }
-
-export const getServerSideProps = async (context) => {
-    const {params} = context;
-    const res = await fetch(`http://localhost:5000/products/${params.productId}`);
-    const data = await res.json();
-    return {
-        props: {
-            product: data,
-        },
-    }
-}

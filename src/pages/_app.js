58a40c39e2f6f4b7cb3551c54ede17434a93638f
Arman-Kabir <armankabir8@gmail.com>
2023-08-06 17:34:16 +0600

    nextauth added and wrapped the _app components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 // import '@/styles/globals.css'
 
 import store from "@/redux/store"
-import { Provider } from "react-redux"
+import { Provider } from "react-redux";
+import { SessionProvider } from "next-auth/react"
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <Provider store={store}>
-      {getLayout(<Component {...pageProps} />)}
-    </Provider>
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        {getLayout(<Component {...pageProps} />)}
+      </Provider>
+    </SessionProvider>
   )
 }

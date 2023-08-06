34df3bbc5f77aad8520c6ee1c5e6f237842b5459
Arman-Kabir <armankabir8@gmail.com>
2023-08-06 15:34:20 +0600

    category connected to server api

--- src/pages/category/[category].js
+++ src/pages/category/[category].js
@@ -4,10 +4,10 @@ import { useRouter } from "next/router";
 
 
 const Category = ({ allProducts }) => {
-  const router = useRouter();
+  // const router = useRouter();
   console.log(allProducts);
   return (
-    <div>Category:{router.query.category}
+    <div>
       <AllProducts products={allProducts}>
       </AllProducts>
     </div>
@@ -25,12 +25,12 @@ Category.getLayout = function getLayout(page) {
 }
 
 export const getServerSideProps = async (context) => {
-  const { category } = context.query;
-  const res = await fetch(`http://localhost:3000/api/categories/${category}`);
+  const { category } = context.params;
+  const res = await fetch(`https://pc-maker-backend.vercel.app/categories/${category}`);
   const data = await res.json();
   return {
     props: {
-      allProducts: data.data,
+      allProducts: data,
     }
   }
 }
